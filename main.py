7fcc3b303e761467a76c0d275eb38bc10a7f1e2e
Fredrik-L <fredrik.liliequist@yh.nackademin.se>
2020-03-05 09:13:28 +0100

    row and column

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 def createGrid():
     grid = []
     i = 0
@@ -8,5 +9,41 @@ def createGrid():
             j = j + 1
         i = i + 1
     return grid
-grid = createGrid()
-print(grid)
+
+def createRow(a, b, c):
+    global grid
+    row = [a,b,c]
+    grid.append(row)
+    
+def possible(x,y,n):
+    for i in range(3):
+        if n == grid[x][i]:
+            print("Denna finns på rad")
+            return False
+        if n == grid[i][y]:
+            print("Dena finns på kolumnen")
+            return False
+        
+    for i in range(3):
+        for j in range(3):
+            if n == grid[i][j]:
+                print("Detta finns i boxen")
+                return False
+    return True
+
+grid = []
+createRow(1,2,3)
+createRow(4,0,6)
+createRow(7,8,9)
+
+solved = False
+while(solved == False):
+    n = 1
+    print(n)
+    if (possible(1,1,n) == True):
+        grid[1][1] = n
+        solved = True
+    print("\n",grid[0], "\n", grid[1], "\n", grid[2])
+    n = n + 1
+    if n == 20:
+        solved = True
